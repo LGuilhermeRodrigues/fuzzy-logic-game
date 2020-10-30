fc74b77729abe687ecb8d74ead37a99c54a6dbe4
Luis Guilherme Silva Rodrigues <guilherme.rodrigues@unesp.br>
2020-10-30 16:58:50 -0300

    passing player position to main function

--- hand_tracking.py
+++ hand_tracking.py
@@ -1,96 +1,81 @@
 import cv2
-import imutils as imutils
 import numpy as np
-import math
 
-image_hsv = None   # global ;(
 image_src = None
-pixel = (20,60,80) # some stupid default
-upper = None
-lower = None
+pixel = (20, 60, 80)  # some random default
+upper = (20, 60, 80)
+lower = (20, 60, 80)
+
 
 # mouse callback function
-def pick_color(event,x,y,flags,param):
-    global image_hsv, pixel, image_src, upper, lower
+def pick_color(event, x, y, flags, param):
+    global pixel, image_src, upper, lower
     image_hsv = cv2.cvtColor(image_src, cv2.COLOR_BGR2HSV)
     if event == cv2.EVENT_LBUTTONDOWN:
-        pixel = image_hsv[y,x]
+        pixel = image_hsv[y, x]
+        # you might want to adjust the ranges(+-10, etc):
+        upper = np.array([pixel[0] + 20, pixel[1] + 20, pixel[2] + 80])
+        lower = np.array([pixel[0] - 20, pixel[1] - 20, pixel[2] - 80])
 
-        #you might want to adjust the ranges(+-10, etc):
-        upper =  np.array([pixel[0] + 20, pixel[1] + 20, pixel[2] + 80])
-        lower =  np.array([pixel[0] - 20, pixel[1] - 20, pixel[2] - 80])
-        print(pixel, lower, upper)
 
-        image_mask = cv2.inRange(image_hsv,lower,upper)
-        cv2.imshow("mask",image_mask)
+def show_mask(roi):
+    hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower, upper)
+
+    kernel = np.ones((3, 3), np.uint8)
+    mask = cv2.dilate(mask, kernel, iterations=8)
+    mask = cv2.GaussianBlur(mask, (5, 5), 100)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = contours
+    if len(cnts):
+        cnt = max(cnts, key=lambda x: cv2.contourArea(x))
+        M = cv2.moments(cnt)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        # draw the contour and center of the shape on the image
+        cv2.drawContours(mask, [cnt], -1, (0, 255, 0), 2)
+        cv2.circle(mask, (cX, cY), 7, (0, 255, 255), -1)
+        cv2.putText(mask, "center", (cX - 20, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+    else:
+        cX = 0
+        cY = 0
+
+    # show the windows
+    cv2.imshow('mask', mask)
+    return cX, cY
 
 
 def main():
     cap = cv2.VideoCapture(0)
-    # COLOR PICKER
-    global image_hsv, pixel, image_src, upper,lower # so we can use it in mouse callback
-
-    ret, image_src = cap.read()
-    cv2.imshow("bgr", image_src)
-    cv2.setMouseCallback('bgr', pick_color)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    kernel = np.ones((3, 3), np.uint8)
-
+    global pixel, image_src, upper, lower  # so we can use it in mouse callback
     frame_number = 0
-
-    while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+    while cv2.getWindowProperty('frame', cv2.WND_PROP_VISIBLE) >= 1:
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
+        image_src = frame
 
         # define region of interest
-        roi = frame[100:300, 100:300]
+        roi = frame[40:480-40, 120:640-120]
+        player_x, player_y = show_mask(roi)
+        cv2.putText(frame, f'Player: {player_x},{player_y}', (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 10, 10), 2)
+        # camera resolution = 480,640
+        cv2.rectangle(frame, (120, 40), (640-120, 480-40), (255, 10, 10), 0)
 
-        cv2.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
-        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-        mask = cv2.inRange(hsv, lower, upper)
-
-        mask = cv2.dilate(mask, kernel, iterations=8)
-        mask = cv2.GaussianBlur(mask, (5, 5), 100)
-        contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        #cnts = imutils.grab_contours(contours)
-        cnts = contours
-        if len(cnts):
-            cnt = max(cnts, key=lambda x: cv2.contourArea(x))
-            """epsilon = 0.0005 * cv2.arcLength(cnt, True)
-            approx = cv2.approxPolyDP(cnt, epsilon, True)
-            hull = cv2.convexHull(cnt)
-            areahull = cv2.contourArea(hull)
-            areacnt = cv2.contourArea(cnt)
-            arearatio = ((areahull - areacnt) / areacnt) * 100
-            hull = cv2.convexHull(approx, returnPoints=False)
-            defects = cv2.convexityDefects(approx, hull)
-            l = 0
-            # cv2.drawContours(frame, hull, 0, (255, 0, 0), 1, 8)"""
-            M = cv2.moments(cnt)
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            # draw the contour and center of the shape on the image
-            cv2.drawContours(mask, [cnt], -1, (0, 255, 0), 2)
-            cv2.circle(mask, (cX, cY), 7, (0, 255, 255), -1)
-            cv2.putText(mask, "center", (cX - 20, cY - 20),cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
-
-        frame_number += 1
-        cv2.putText(frame, str(frame_number), (20, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-
-        # show the windows
-        cv2.imshow('mask', mask)
         cv2.imshow('frame', frame)
+        cv2.setMouseCallback('frame', pick_color)
 
         k = cv2.waitKey(5) & 0xFF
         if k == 27:
+            break
+        if k == ord("q"):
             break
 
     cv2.destroyAllWindows()
     cap.release()
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()
